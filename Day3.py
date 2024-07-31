b0e737e4601f0df6b18f0a67b9de8606f8489f53
Venkata Sai Vidhyanandh <86034369+Vidhyanandh@users.noreply.github.com>
2024-07-31 11:56:40 +0530

    DAY-3 CHALLENGE COMPLETED

--- Day3.py
+++ Day3.py
@@ -11,3 +11,16 @@ def kangaroo(x1, v1, x2, v2):
             return "YES"
         else:
             return "NO"
+
+# APPROACH-2
+def kangaroo(x1,v1,x2,v2):
+    if x1<x2 and v1<v2:
+        return 'NO'
+    elif x2<x1 and v2<v1:
+        return 'NO'
+    while(x1<=x2):
+        x1+=v1
+        x2+=v2
+        if x1==x2:
+            return'YES'
+    return'NO'
