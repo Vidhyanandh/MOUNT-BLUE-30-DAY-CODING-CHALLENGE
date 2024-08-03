ebf455d39547fe7ade30a8eec574cca5a8d98360
Venkata Sai Vidhyanandh <86034369+Vidhyanandh@users.noreply.github.com>
2024-08-03 12:57:50 +0530

    Update Day-6.py

--- Day-6.py
+++ Day-6.py
@@ -12,3 +12,23 @@ def migratoryBirds(arr):
             result.append(key)
     result.sort()
     return result[0] 
+
+#APPROACH-2
+
+def migratoryBirds(arr):
+    bird_count={}
+    max_count=0
+    smallest_bird_id= None
+    for i in arr:
+        if i in bird_count:
+            bird_count[i]+=1
+        else:
+            bird_count[i]=1
+    for i,j in bird_count.items():
+        if j>max_count:
+            max_count=j
+            smallest_bird_id=i
+        elif j==max_count:
+            if smallest_bird_id is None or i<smallest_bird_id:
+                smallest_bird_id=i
+    return smallest_bird_id
