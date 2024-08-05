6ce2c18660a6f5d40bb6ee575608b188e6882d88
Venkata Sai Vidhyanandh <86034369+Vidhyanandh@users.noreply.github.com>
2024-08-05 13:05:07 +0530

    Update day-7.py

--- day-7.py
+++ day-7.py
@@ -1,8 +1,5 @@
 def pageCount(n, p):
     front_count=p//2
-    if n % 2 == 0:
-        back_count = (n - p + 1) // 2
-    else:
-        back_count = (n - p) // 2
+    back_count=n//2-p//2
         
     return min(front_count,back_count)
